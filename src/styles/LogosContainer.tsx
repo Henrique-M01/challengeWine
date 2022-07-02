e483146b1e15dacaf50f157f2861e866d7c7537e
henrique <hickm018@gmail.com>
2022-07-02 13:57:34 -0300

    fix: add atyle header and logos

---
+++ src/styles/LogosContainer.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const LogosContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 300px;
+`;
+
+export default LogosContainer;
